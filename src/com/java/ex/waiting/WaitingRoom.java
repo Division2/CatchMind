dc1b0b04e2f0ceed4208b4c8b7b807518cc13894
박성동 <freegunz2@naver.com>
2020-11-12 01:13:32 +0900

    임시

--- src/com/java/ex/waiting/WaitingRoom.java
+++ src/com/java/ex/waiting/WaitingRoom.java
@@ -5,8 +5,13 @@ import java.awt.Container;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableModel;
 
 public class WaitingRoom extends JFrame{
 	
@@ -17,8 +22,12 @@ public class WaitingRoom extends JFrame{
 	JButton btnUpdateProfile = null;
 	JButton btnLogout = null;
 	JButton btnExit = null;
+	DefaultTableModel roomModel = null;
+	DefaultTableModel userModel = null;
 	JTable tabRoom = null;
 	JTable tabUser = null;
+	JTextArea chattingRoom = null;
+	JTextField chatting = null;
 	
 	public WaitingRoom() {
 		Container ct = getContentPane();
@@ -29,7 +38,7 @@ public class WaitingRoom extends JFrame{
 		btnCreateRoom = new JButton("방만들기");
 		btnRanking = new JButton("랭킹");
 		btnUpdateProfile = new JButton("내 정보 수정");
-		
+
 		btnJoinRoom.setBounds(20, 20, 100, 50);
 		btnCreateRoom.setBounds(140, 20, 100, 50);
 		btnRanking.setBounds(260, 20, 100, 50);
@@ -39,7 +48,60 @@ public class WaitingRoom extends JFrame{
 		waitingRoomPanel.add(btnCreateRoom);
 		waitingRoomPanel.add(btnRanking);
 		waitingRoomPanel.add(btnUpdateProfile);
-				
+		
+		//대기방 테이블
+		String[] roomField = { "NO", "제목", "방장", "인원", "비고" };
+		roomModel = new DefaultTableModel(roomField, 0) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		
+		tabRoom = new JTable(roomModel);
+		tabRoom.setRowHeight(50);
+		tabRoom.getColumnModel().getColumn(0).setPreferredWidth(50);
+		tabRoom.getColumnModel().getColumn(1).setPreferredWidth(300);
+		tabRoom.getColumnModel().getColumn(2).setPreferredWidth(100);
+		tabRoom.getTableHeader().setReorderingAllowed(false);
+		JScrollPane scroll1 = new JScrollPane(tabRoom);
+		scroll1.setBounds(20, 90, 600, 350);
+		waitingRoomPanel.add(scroll1);
+		//대기방 테이블
+		
+		//유저목록 테이블
+		String[] userField = { "닉네임", "레벨", "위치" };
+		userModel = new DefaultTableModel(userField, 0) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		
+		tabUser = new JTable(userModel);
+		tabUser.setRowHeight(25);
+		tabUser.getTableHeader().setReorderingAllowed(false);
+		JScrollPane scroll2 = new JScrollPane(tabUser);
+		scroll2.setBounds(640, 90, 350, 350);
+		scroll2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		waitingRoomPanel.add(scroll2);
+		//유저목록 테이블
+		
+		// 채팅창 및 채팅 입력창
+		chattingRoom = new JTextArea();
+		chattingRoom.setEditable(false);
+		chattingRoom.setBounds(22, 465, 653, 184);
+		JScrollPane scroll3 = new JScrollPane(chattingRoom);
+		scroll3.setBounds(20, 450, 600, 200);
+		scroll3.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		chatting = new JTextField();
+		chatting.setBounds(80, 660, 450, 20);
+		waitingRoomPanel.add(scroll3);
+		waitingRoomPanel.add(chatting);
+		// 채팅창 및 채팅 입력창
+		
+		
+		
 		ct.add(waitingRoomPanel);
 		
 		setTitle("캐치마인드 대기실");
